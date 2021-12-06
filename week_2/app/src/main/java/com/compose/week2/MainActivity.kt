ccbeb4e7235e962db84e32117a0af24cfd1ca107
jksong <wlsrhkd4023@gmail.com>
2021-12-06 17:43:03 +0900

    pass class of 8

--- week_2/app/src/main/java/com/compose/week2/MainActivity.kt
+++ week_2/app/src/main/java/com/compose/week2/MainActivity.kt
@@ -21,12 +21,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
+import com.compose.week2.ui.firstBaselineToTop
 import com.compose.week2.ui.theme.LayoutsCodelabTheme
 import kotlinx.coroutines.launch
 
@@ -65,11 +68,14 @@ fun LayoutCodelab() {
     }
 }
 
+@Preview
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
@@ -200,6 +206,65 @@ fun Buttons() {
             Image(painter = painterResource(id = R.drawable.ic_baseline_favorite_24), contentDescription = "")
             Spacer(modifier = Modifier.width(4.dp))
             Text(text = "Button")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    Text("Hi there!", Modifier.padding(top = 32.dp))
+}
+
+/*@Composable
+fun CustomLayout(
+    modifier: Modifier = Modifier,
+    // custom layout attributes
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // measure and position children given constraints logic here
+    }
+}*/
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
         }
     }
 }
