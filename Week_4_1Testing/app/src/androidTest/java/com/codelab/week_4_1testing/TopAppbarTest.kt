411a6580d3749fac11e3754db99d7857877871f8
jksong <wlsrhkd4023@gmail.com>
2021-12-10 11:06:26 +0900

    Add week of 4_1 testing

---
+++ Week_4_1Testing/app/src/androidTest/java/com/codelab/week_4_1testing/TopAppbarTest.kt
@@ -0,0 +1,81 @@
+package com.codelab.week_4_1testing
+
+import androidx.compose.material.Text
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.text.toUpperCase
+import com.codelab.week_4_1testing.ui.RallyScreen
+import com.codelab.week_4_1testing.ui.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+
+class TopAppbarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun myTest() {
+        composeTestRule.setContent {
+            Text("You can set any Compose content!")
+        }
+    }
+
+    @Test
+    fun rallyTopAppbarTest() {
+        val allScreens = RallyScreen.values().toList()
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun rallyTopAppbarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppbarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        //composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.toUpperCase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+
+        /*composeTestRule
+            .onNodeWithText(RallyScreen.Accounts.name)
+            .assertExists()*/
+    }
+}
---
+++ Week_4_1Testing/app/src/main/java/com/codelab/week_4_1testing/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.codelab.week_4_1testing.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
